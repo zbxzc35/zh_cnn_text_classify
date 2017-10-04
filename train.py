29db474f350270ba654b4001fa7459598b5dd817
gary <epcilon@126.com>
2017-10-05 07:28:57 +0800

    solve memory error

--- train.py
+++ train.py
@@ -14,15 +14,15 @@ from text_cnn import TextCNN
 # =======================================================
 
 # Data loading parameters
-tf.flags.DEFINE_float("dev_sample_percentage", .15, "Percentage of the training data to use for validation")
+tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
 
 tf.flags.DEFINE_string("data_dir", "./data/processed/", "Data source for classification.")
 
 tf.flags.DEFINE_integer("num_labels", None, "Number of labels for data. (default: None)")
-tf.flags.DEFINE_integer("max_document_len", None, "Max document lenth. (default: None)")
+tf.flags.DEFINE_integer("max_document_len", 2500, "Max document lenth. (default: None)")
 
 # Model hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 256, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 512, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-spearated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
