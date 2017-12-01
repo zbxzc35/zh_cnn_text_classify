130a1c441a5ea4f5bbde7c6a069ea6fbda116155
GaryZ <garyzhou@astri.org>
2017-12-01 10:55:05 +0800

    data

--- eval.py
+++ eval.py
@@ -21,7 +21,7 @@ tf.flags.DEFINE_string("data_dir", "./data/processed/testing/", "Test text data 
 
 # Eval Parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "./runs/2017-11-28T09:12:15.017009/checkpoints/", "Checkpoint directory from training run")
+tf.flags.DEFINE_string("checkpoint_dir", "./runs/2017-11-30T16:59:02.518790/checkpoints/", "Checkpoint directory from training run")
 tf.flags.DEFINE_boolean("eval_train", True, "Evaluate on all training data")
 tf.flags.DEFINE_string("wordembedding_name", "trained_word2vec.model.all", "Word embedding model name. (default: trained_word2vec.model)")
 tf.flags.DEFINE_boolean("word_segment", False, "Whether do word segmentation. (default: False)")
